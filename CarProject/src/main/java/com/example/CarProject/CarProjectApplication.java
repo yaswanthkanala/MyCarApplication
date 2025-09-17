3a4d49e8d8592ec19d65aa1bf15652ba247a3507
yaswanthkanala <kanalayaswanth1996@gmail.com>
2025-09-17 13:13:55 +0530

    pushing this car project

---
+++ CarProject/src/main/java/com/example/CarProject/CarProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarProjectApplication.class, args);
+	}
+
+}
